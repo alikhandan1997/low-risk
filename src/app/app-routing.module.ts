9fa8c5383723f3540fcfdb0ab6c3f9180c170b95
alikhandan1997 <isateas2020@gmail.com>
2020-05-16 23:37:30 +0430

    adding admin and main page and children dashboard for admin and home-page for main page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,33 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AdminComponent } from './layouts/admin/admin.component';
+import { DashboardComponent } from './modules/dashboard/dashboard.component';
+import { MainComponent } from './layouts/main/main.component';
+import { HomePageComponent } from './modules/home-page/home-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'admin',
+    component: AdminComponent,
+    children: [
+      {
+        path: '',
+        component: DashboardComponent
+      }
+    ]
+  },{
+    path: '',
+    component: MainComponent,
+    children: [
+      {
+        path: '',
+        component: HomePageComponent
+      }
+    ]
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
