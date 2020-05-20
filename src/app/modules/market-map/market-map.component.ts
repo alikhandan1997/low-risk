3aac9d363067893510a404ae1b5dc003b0efe8e0
alikhandan1997 <isateas2020@gmail.com>
2020-05-20 05:06:03 +0430

    adding market map module for market map and table module for indexes and stock tables

---
+++ src/app/modules/market-map/market-map.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-market-map',
+  templateUrl: './market-map.component.html',
+  styleUrls: ['./market-map.component.scss']
+})
+export class MarketMapComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
