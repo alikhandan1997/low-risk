6ecb8bb33f93cf4b72dcd7a675ef602126ec34ee
alikhandan1997 <isateas2020@gmail.com>
2020-05-26 03:13:07 +0430

    making learn component like detail compnent except the video iframe tag

--- src/app/modules/learn/learn.component.ts
+++ src/app/modules/learn/learn.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-learn',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LearnComponent implements OnInit {
 
-  constructor() { }
+  isFilm: boolean = false;
+  isArticle: boolean = false;
+
+  constructor(private sanitizer: DomSanitizer) { }
 
   ngOnInit(): void {
+    if(window.location.href.split('/')[4] == 'film') {
+      this.isFilm = true;
+      console.log(this.isFilm);
+    } else if(window.location.href.split('/')[4] == 'article') {
+      this.isArticle = true;
+      console.log(this.isArticle);
+    }
+  }
+
+  getEmbedUrl() {
+    return this.sanitizer.bypassSecurityTrustResourceUrl('assets/video.mp4');
   }
 
 }
