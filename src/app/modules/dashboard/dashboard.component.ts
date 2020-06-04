fb905cb013e02ee01654be077dc28dd923b389be
alikhandan1997 <isateas2020@gmail.com>
2020-06-05 01:14:59 +0430

    removing the previous editor

--- src/app/modules/dashboard/dashboard.component.ts
+++ src/app/modules/dashboard/dashboard.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit, ViewChild, ElementRef, Renderer2 } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { Component, OnInit} from '@angular/core';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -7,71 +6,9 @@ import { FormGroup, FormControl } from '@angular/forms';
 })
 export class DashboardComponent implements OnInit {
 
-  @ViewChild('content_box') content_box: ElementRef;
-
-  // form = new FormData();
-  // public imagePath;
-  // imgURL: any;
-  // public message: string;
-
-  constructor( private renderer: Renderer2) { }
-
-
-userForm = new FormGroup({
-  name: new FormControl(),
-  age: new FormControl('20')
-});
+  constructor() { }
 
   ngOnInit(): void {
   }
-
-  addImage() {
-    this.content_box.nativeElement.innerHTML = `
-    Age: <input formControlName="age"  placeholder="Enter Age">
-    `;
-    // const input: HTMLInputElement = this.renderer.createElement('input');
-    // input.setAttribute('formControlName', 'age');
-    // this.renderer.appendChild(this.content_box.nativeElement, input);
-    // const inputContainer: HTMLDivElement = this.renderer.createElement('div');
-    // const imageContainer: HTMLDivElement = this.renderer.createElement('div');
-    // const input: HTMLInputElement = this.renderer.createElement('input');
-    // const image: HTMLImageElement = this.renderer.createElement('img');
-
-    // inputContainer.className = "col-lg-12";
-    // imageContainer.className = "col-lg-12";
-
-    // input.type = 'file';
-    // input.addEventListener("change", function loadFile(event) {
-    //   // console.log((<HTMLInputElement>event.target).files)
-    //   this.form.append('photo', `${(<HTMLInputElement>event.target).files}`)
-    //   image.src = URL.createObjectURL((<HTMLInputElement>event.target).files[0]);
-    //   image.onload = function() {
-    //     URL.revokeObjectURL(image.src) // free memory
-    //   }
-    //   imageContainer.appendChild(image);
-    // })
-
-    // this.renderer.appendChild(this.content_box.nativeElement, inputContainer);
-    // inputContainer.appendChild(input);
-    // this.renderer.appendChild(this.content_box.nativeElement, imageContainer);
-  }
-
-  addText() {
-    // const textContainer: HTMLDivElement = this.renderer.createElement('div');
-    // const textarea: HTMLTextAreaElement = this.renderer.createElement('textarea');
-
-    // textContainer.className = "col-lg-12";
-    // textarea.addEventListener("change", function loadFile(event) {
-    //   // console.log((<HTMLInputElement>event.target).value)
-
-    // })
-
-    // this.renderer.appendChild(this.content_box.nativeElement, textContainer);
-    // textContainer.appendChild(textarea);
-  }
-
-  onFormSubmit(): void {
-    console.log('Name:' + this.userForm.get('name').value);
-}
 
 }
