c7ebe43dbc658d0c9a3734fcf16942a5c2c75e43
alikhandan1997 <isateas2020@gmail.com>
2020-06-12 12:58:24 +0430

    adding new component for analysis detail

---
+++ src/app/modules/analysis-detail/analysis-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-analysis-detail',
+  templateUrl: './analysis-detail.component.html',
+  styleUrls: ['./analysis-detail.component.scss']
+})
+export class AnalysisDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
