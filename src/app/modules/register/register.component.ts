3dee960c6dd7c2562b658966cfebea7770cd1112
alikhandan1997 <isateas2020@gmail.com>
2020-06-13 00:41:59 +0430

    adding api to the register module

--- src/app/modules/register/register.component.ts
+++ src/app/modules/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ServicesService } from 'src/app/modules/services/services.service';
 
 
 @Component({
@@ -8,15 +9,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  name: string;
+  mobile;
+  password;
+  conf_password;
+  captcha;
+  dataObj: Object;
 
-  constructor() { }
+  captcha_image: string;
+  captcha_key: string;
+
+
+  constructor( private http: ServicesService) { }
 
   ngOnInit(): void {
+    this.getCaptcha();
+  }
+
+  getCaptcha(){
+    this.http.postCaptcha().subscribe((data) => {
+      this.captcha_image = data["result"].captcha_image;
+      this.captcha_key = data["result"].captcha_key;
+    })
   }
 
   click(){
-    console.log(this.name)
+    console.log('name',this.mobile,'pass',this.password,'conf',this.conf_password,'captcha', this.captcha)
+    this.dataObj = {
+      "mobile": this.mobile,
+      "password": this.password,
+      "confirm_password": this.conf_password,
+      "captcha_key": this.captcha_key,
+      "captcha_value": this.captcha
+    }
+    this.postLogin();
+  }
+
+  postLogin(){
+    this.http.postRegister(this.dataObj).subscribe((data) => {
+      console.log(data)
+    })
   }
 
 }
