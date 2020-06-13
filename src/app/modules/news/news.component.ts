68c2e7dd3971ac864ab641ec3e8c22b53ffeab6a
alikhandan1997 <isateas2020@gmail.com>
2020-06-13 22:27:32 +0430

    changing learn detail component

--- src/app/modules/news/news.component.ts
+++ src/app/modules/news/news.component.ts
@@ -18,9 +18,9 @@ export class NewsComponent implements OnInit {
 
   ngOnInit(): void {
 
-    if(window.location.href.split('/')[4] == 'film') {
+    if(window.location.href.split('/')[4] == 'film' && window.location.href.split('/').length == 5) {
       this.isFilm = true;
-    } else if(window.location.href.split('/')[4] == 'article'){
+    } else if(window.location.href.split('/')[4] == 'article' && window.location.href.split('/').length == 5){
       this.isArticle = true;
     } else if(window.location.href.split('/')[3] == 'news' && window.location.href.split('/').length == 4){
       this.isNews = true;
