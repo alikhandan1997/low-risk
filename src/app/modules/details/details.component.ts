5cc976aaecb8332ad4118c7210e0212bad7dd7a3
alikhandan1997 <isateas2020@gmail.com>
2020-06-14 20:38:16 +0430

    adding api function and set data to its values

--- src/app/modules/details/details.component.ts
+++ src/app/modules/details/details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ServicesService } from '../services/services.service';
 
 @Component({
   selector: 'app-details',
@@ -7,9 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailsComponent implements OnInit {
 
-  constructor() { }
+  Type: string;
+  mainImage: string;
+  postTitle: string;
+  postLittleDescription: string;
+  postContent: string;
+  postId: string;
+
+  constructor(private http: ServicesService) { }
 
   ngOnInit(): void {
+    // check if is news
+    if(window.location.href.split('/')[3] == 'news') {
+      this.Type = 'news';
+      this.postId = window.location.href.split('/')[4];
+    }
+    console.log(this.postId)
+
+    this.getData();
+  }
+
+  getData(){
+    if(this.Type == 'news'){
+      this.http.getNews(this.postId).subscribe((data) => {
+        console.log(data['result']);
+        this.mainImage = data['result']['image'];
+        this.postTitle = data['result']['title'];
+        this.postLittleDescription = data['result']['description'];
+        this.postContent = data['result']['content'];
+      });
+    }
   }
 
 }
