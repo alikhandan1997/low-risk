3b6db6c71cca9e1e8a03f30d9fff7f86ff6a1652
alikhandan1997 <isateas2020@gmail.com>
2020-06-16 22:56:16 +0430

    adding list example to the list component for testing

--- src/app/modules/list-items/list-items.component.ts
+++ src/app/modules/list-items/list-items.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ServicesService } from '../services/services.service';
 
 @Component({
   selector: 'app-list-items',
@@ -7,9 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListItemsComponent implements OnInit {
 
-  constructor() { }
+  Type: string = "";
+  apiData: string = "";
+
+  constructor(private http: ServicesService) { }
 
   ngOnInit(): void {
+
+    if(window.location.href.split('/')[4] == 'learn') {
+      this.Type = 'learn';
+    } else if(window.location.href.split('/')[4] == 'news') {
+      this.Type = 'news';
+    } else if(window.location.href.split('/')[4] == 'analysis') {
+      this.Type = 'analysis';
+    }
+
+    this.getData();
+  }
+
+  getData() {
+    if(this.Type == 'learn') {
+      this.http.getEducations(this.apiData).subscribe((data) => {
+        console.log(data);
+      });
+    } else if (this.Type == 'news') {
+      this.http.getNews(this.apiData).subscribe((data) => {
+        console.log(data);
+      });
+    } else if(this.Type == 'analysis'){}
   }
 
 }
