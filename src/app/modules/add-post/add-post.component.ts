f493ed9e7f6a2beadbbee6061bdfa22fa3d8727a
alikhandan1997 <isateas2020@gmail.com>
2020-06-16 21:02:59 +0430

    adding file and video input the add post file

--- src/app/modules/add-post/add-post.component.ts
+++ src/app/modules/add-post/add-post.component.ts
@@ -70,13 +70,31 @@ export class AddPostComponent implements OnInit {
   }
 
   readURL(event: Event): void {
+
     if ((<HTMLInputElement>event.target).files && (<HTMLInputElement>event.target).files[0]) {
         const file = (<HTMLInputElement>event.target).files[0];
         const reader = new FileReader();
         reader.onload = () => {
           this.imageSrc = reader.result;
+          this.mainImage = reader.result;
         }
         reader.readAsDataURL(file);
+    }
+  }
+
+  readFile(event: Event, fileType: string): void {
+
+    if ((<HTMLInputElement>event.target).files && (<HTMLInputElement>event.target).files[0]) {
+      const file = (<HTMLInputElement>event.target).files[0];
+      const reader = new FileReader();
+      reader.onload = () => {
+        if(fileType == 'video') {
+          this.mainVideo = reader.result;
+        } else if(fileType == 'file') {
+          this.mainFile = reader.result;
+        }
+      }
+      reader.readAsDataURL(file);
     }
   }
 
