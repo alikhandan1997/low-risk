1e247855140aba80c1117c5b3f5731ee6be2f85e
alikhandan1997 <isateas2020@gmail.com>
2020-07-09 20:54:33 +0430

    adding data from api to the component

--- src/app/modules/learn/learn.component.ts
+++ src/app/modules/learn/learn.component.ts
@@ -1,18 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
 import { ServicesService } from '../services/services.service';
 
 @Component({
   selector: 'app-learn',
   templateUrl: './learn.component.html',
-  styleUrls: ['./learn.component.scss']
+  styleUrls: ['./learn.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class LearnComponent implements OnInit {
 
   isFilm: boolean = false;
   isArticle: boolean = false;
   isDetail: boolean = false;
-  apiData: string = '';
+  postId;
+  apiData = '';
+  Post = [];
+  otherPost = [];
+  otherPostList = [];
 
   constructor(
     private sanitizer: DomSanitizer,
@@ -28,6 +33,7 @@ export class LearnComponent implements OnInit {
     } else if(window.location.href.split('/')[4] == "film") {
       this.isFilm = true;
     }
+    this.postId = window.location.href.split('/')[5];
 
     this.getData();
   }
@@ -37,15 +43,38 @@ export class LearnComponent implements OnInit {
   }
 
   getData() {
-    if(this.isFilm) {
-      this.http.getEducations(this.apiData).subscribe((data) => {
-        console.log(data);
-      });
-    } else if(this.isArticle){
-      this.http.getEducations(this.apiData).subscribe((data) => {
-        console.log(data);
-      });
-    }
+    this.http.getEducations(this.postId).subscribe((data) => {
+      this.Post.push(data['result']);
+    });
+
+
+    this.apiData = '?page_size=3&last';
+    this.http.getEducations(this.apiData).subscribe((data) => {
+      console.log(data);
+      this.otherPost = data['result']['results'];
+      for(let i=0; i<this.otherPost.length; i++) {
+        if(this.otherPost[i]['id'] == this.postId) {
+          this.otherPost.splice(i, 1);
+        }
+      }
+      if(this.otherPost.length == 3) {
+        this.otherPost.pop();
+      }
+    });
+
+    this.apiData = '?page_size=6&last';
+    this.http.getEducations(this.apiData).subscribe((data) => {
+      console.log(data);
+      this.otherPostList = data['result']['results'];
+      for(let i=0; i<this.otherPostList.length; i++) {
+        if(this.otherPostList[i]['id'] == this.postId) {
+          this.otherPostList.splice(i, 1);
+        }
+      }
+      if(this.otherPostList.length == 6) {
+        this.otherPostList.pop();
+      }
+    });
   }
 
 }
