d281f2854128e48abb0368f512bfa92b7596baec
alikhandan1997 <isateas2020@gmail.com>
2020-07-13 20:05:41 +0430

    adding token handler to the app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ServicesService } from './modules/services/services.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'low-risk';
+export class AppComponent implements OnInit {
+
+  apiData = '';
+
+  constructor(private http: ServicesService) {}
+
+  ngOnInit() {
+    this.getData();
+  }
+
+  getData() {
+    this.http.getAdminNews(this.apiData).subscribe((data) => {
+    },
+    (error) => {
+      console.log(error['status'])
+      if(error['status'] == 401) {
+        localStorage.clear();
+      }
+    })
+  }
 }
