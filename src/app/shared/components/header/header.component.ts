5b73fe7d0a8997aae9fe035365966b499e80df9d
alikhandan1997 <isateas2020@gmail.com>
2020-07-13 20:06:54 +0430

    change the size of the login pop up

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -28,7 +28,7 @@ export class HeaderComponent implements OnInit {
   openDialog(): void {
     const dialogRef = this.dialog.open(LoginComponent, {
       width: '350px',
-      height: '400px'
+      height: '430px'
     });
 
     dialogRef.afterClosed().subscribe(result => {
