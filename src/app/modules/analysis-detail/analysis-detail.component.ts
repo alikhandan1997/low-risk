4ae03c7020fb2a90eb9882932f208b8ff97e8208
alikhandan1997 <isateas2020@gmail.com>
2020-07-20 23:27:45 +0430

    getting data from api for post detail similar posts and the new list

--- src/app/modules/analysis-detail/analysis-detail.component.ts
+++ src/app/modules/analysis-detail/analysis-detail.component.ts
@@ -8,19 +8,58 @@ import { ServicesService } from '../services/services.service';
 })
 export class AnalysisDetailComponent implements OnInit {
 
-  postId
+  apiData;
+  postId;
+  postData = [];
+  Date;
+  analysisTypeNumber = '';
+  analysisType = '';
+  otherAnalysis = [];
+  newList = [];
 
   constructor(private http: ServicesService) { }
 
   ngOnInit(): void {
     this.postId = window.location.href.split('/')[5];
-
-    console.log(this.postId)
-
     this.getData();
   }
 
   getData(){
+
+    // get the main post data
+    this.http.getAnalysis(this.postId).subscribe((data) => {
+      this.Date = data['result']['created_at'];
+      this.postData.push(data['result']);
+      this.analysisTypeNumber = data['result']['type'];
+      if(data['result']['type'] == 1) {
+        this.analysisType = 'short';
+      } else if(data['result']['type'] == 2){
+        this.analysisType = 'medium';
+      } else if(data['result']['type'] == 3) {
+        this.analysisType = 'long';
+      }
+    });
+
+    // get sub analysis from data
+    this.apiData = `?type=${this.analysisTypeNumber}&page_size=3&last`;
+    this.http.getAnalysis(this.apiData).subscribe((data) => {
+      this.otherAnalysis = data['result']['results'];
+      for(let i=0; i<this.otherAnalysis.length; i++) {
+        if(this.otherAnalysis[i]['id'] == this.postId) {
+          this.otherAnalysis.splice(i, 1);
+        }
+      }
+      if(this.otherAnalysis.length == 3) {
+        this.otherAnalysis.pop();
+      }
+    });
+
+    // get news list from data
+    this.apiData = '?page_size=6&last';
+    this.http.getNews(this.apiData).subscribe((data) => {
+      this.newList = data['result']['results'];
+      console.log(this.newList)
+    });
 
   }
 
